1dbe0ef662dfbecac828313c0db0a151a46bf623
otto <otto.vayrynen@gmail.com>
2022-08-06 02:53:05 +0300

    2.1-2.2

---
+++ osa2/kurssitiedot/src/App.js
@@ -0,0 +1,77 @@
+const Course = (props) => {
+  return (
+    <>
+    <Header course={props.course.name} />
+    <Content parts={props.course.parts} />
+    <Total parts={props.course.parts} />
+  </>
+  )
+}
+
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+
+const Part = (props) => {
+  return (
+    <p> {props.name} {props.ex} </p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      {props.parts.map(element => <Part name={element.name} ex={element.exercises} />)}
+    </>
+  )
+}
+
+const Total = (props) => {
+  let total = 0
+  props.parts.forEach(element => 
+    total += element.exercises )
+  return (
+    <p>
+      Number of <strong>{total}</strong>  exercises 
+</p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
+      }
+    ]
+  }
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App
