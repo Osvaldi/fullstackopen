286ba1486248c2f94fd6561a034cf147873f2876
otto <otto.vayrynen@gmail.com>
2026-01-24 21:29:55 +0200

    3.6

--- osa3/puhelinluettelobackend/index.js
+++ osa3/puhelinluettelobackend/index.js
@@ -65,6 +65,18 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
+  if (!body.number) {
+    return response.status(400).json({ 
+      error: 'number missing' 
+    })
+  }
+
+  if (persons.find(person => person.name === body.name)) {
+    return response.status(400).json({ 
+      error: 'name must be unique' 
+    })
+  }
+
   const person = {
     name: body.name,
     number: body.number || "",
