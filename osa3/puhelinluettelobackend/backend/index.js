348fa3633cbbbbed63af5aaea31f920e84b0d8a0
Otto Vayrynen <otto.vayrynen@gmail.com>
2026-03-04 18:48:55 +0200

    removed cors

--- osa3/puhelinluettelobackend/backend/index.js
+++ osa3/puhelinluettelobackend/backend/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
 const app = express()
 
 let persons = [
@@ -33,7 +32,6 @@ morgan.token('body', req => {
   return ' '
 })
 
-app.use(cors())
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
