6004a13ed003c11500f83d0564b07059aac75f98
Otto Vayrynen <otto.vayrynen@gmail.com>
2026-03-10 13:30:39 +0200

    4.4

--- osa4/bloglist/tests/test.js
+++ osa4/bloglist/tests/test.js
@@ -1,10 +1,29 @@
 const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
+const { testBlogs } = require('./test_helper')
 
 test('dummy returns one', () => {
   const blogs = []
 
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
+})
+
+describe('total likes', () => {
+  const emptyList = []
+  const listWithOneBlog = [testBlogs[1]]
+
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes(emptyList)
+    assert.strictEqual(result, 0)
+  })
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.totalLikes(testBlogs)
+    assert.strictEqual(result, 36)
+  })
 })
--- osa4/bloglist/utils/list_helper.js
+++ osa4/bloglist/utils/list_helper.js
@@ -1,7 +1,15 @@
-const dummy = (blogs) => {
+const dummy = () => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const initialValue = 0
+  return blogs.reduce(
+    (sum, blog) => sum + blog.likes,
+    initialValue)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
