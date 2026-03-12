0f09bea5235b12c83b3e6b074d2c74b6d35d6c08
Otto Vayrynen <otto.vayrynen@gmail.com>
2026-03-13 01:54:02 +0200

    4.23 and refactoring

--- osa4/bloglist/tests/blog_api.test.js
+++ osa4/bloglist/tests/blog_api.test.js
@@ -3,14 +3,18 @@ const { test, after, beforeEach, describe } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-const { testBlogs, blogsInDb } = require('./test_helper')
+const { testBlogs } = require('./test_data')
+const { blogsInDb, logInTestUser } = require('./test_helper')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const api = supertest(app)
 
 describe('when there is initially some blogs saved', () => {
+
   beforeEach(async () => {
     await Blog.deleteMany({})
+    await User.deleteMany({})
     await Blog.insertMany(testBlogs)
   })
 
@@ -45,6 +49,15 @@ describe('when there is initially some blogs saved', () => {
 })
 
 describe('addition of a new blog', () => {
+  let token
+
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    await User.deleteMany({})
+    token = await logInTestUser()
+  })
+
+
   test('a valid blog can be added ', async () => {
     const newBlog = {
       title: 'a valid blog can be added',
@@ -55,12 +68,13 @@ describe('addition of a new blog', () => {
 
     await api
       .post('/api/blogs')
+      .set('Authorization', token)
       .send(newBlog)
       .expect(201)
       .expect('Content-Type', /application\/json/)
 
     const blogsAfterPost = await blogsInDb()
-    assert.strictEqual(blogsAfterPost.length, testBlogs.length + 1)
+    assert.strictEqual(blogsAfterPost.length, 1)
 
     const contents = blogsAfterPost.map((b) => b.title)
     assert(contents.includes('a valid blog can be added'))
@@ -75,6 +89,7 @@ describe('addition of a new blog', () => {
 
     await api
       .post('/api/blogs')
+      .set('Authorization', token)
       .send(newBlog)
       .expect(201)
       .expect('Content-Type', /application\/json/)
@@ -85,7 +100,6 @@ describe('addition of a new blog', () => {
   })
 
   test('blog without title or url returns 400 Bad Request', async () => {
-    const blogsAtStart = await blogsInDb()
 
     const newBlog1 = {
       title: 'blog without title or url returns 400 Bad Request',
@@ -101,26 +115,60 @@ describe('addition of a new blog', () => {
 
     await api
       .post('/api/blogs')
+      .set('Authorization', token)
       .send(newBlog1)
       .expect(400)
 
     await api
       .post('/api/blogs')
+      .set('Authorization', token)
       .send(newBlog2)
       .expect(400)
+  })
 
-    const blogsAtEnd = await blogsInDb()
+  test('blog cannot be added without token', async () => {
+    const newBlog = {
+      title: 'blog cannot be added without token',
+      author: 'Test Author',
+      url: 'https://example.com',
+      likes: 1
+    }
 
-    assert.strictEqual(blogsAtEnd.length, blogsAtStart.length)
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(401)
   })
 })
 
 
 describe('deletion of a blog', () => {
+  let token
+
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    await User.deleteMany({})
+    token = await logInTestUser()
+
+    const blogToInsert = {
+      title: 'Blog to delete',
+      author: 'Test Author',
+      url: 'https://example.com'
+    }
+    await api
+      .post('/api/blogs')
+      .set('Authorization', token)
+      .send(blogToInsert)
+      .expect(201)
+  })
+
   test('succeeds with status code 204 if id is valid', async () => {
     const blogsAtStart = await blogsInDb()
     const blogToDelete = blogsAtStart[0]
-    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .set('Authorization', token)
+      .expect(204)
 
     const blogsAtEnd = await blogsInDb()
     const ids = blogsAtEnd.map(b => b.id)
@@ -131,10 +179,26 @@ describe('deletion of a blog', () => {
 })
 
 describe('update of a blog', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    await User.deleteMany({})
+
+    const blogToInsert = {
+      title: 'Blog to update',
+      author: 'Test Author',
+      url: 'https://example.com',
+      likes: 5,
+    }
+    await Blog.insertMany([blogToInsert])
+  })
+
   test('succeeds with status code 200 if id is valid', async () => {
     const blogsAtStart = await blogsInDb()
     const blogToUpdate = blogsAtStart[0]
-    await api.put(`/api/blogs/${blogToUpdate.id}`).send({ ...blogToUpdate, likes: blogToUpdate.likes + 1 }).expect(200)
+    await api
+      .put(`/api/blogs/${blogToUpdate.id}`)
+      .send({ ...blogToUpdate, likes: blogToUpdate.likes + 1 })
+      .expect(200)
 
     const blogsAtEnd = await blogsInDb()
     const updatedBlog = blogsAtEnd.find(b => b.id === blogToUpdate.id)
--- osa4/bloglist/tests/test_helper.js
+++ osa4/bloglist/tests/test_helper.js
@@ -1,56 +1,9 @@
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const supertest = require('supertest')
+const app = require('../app')
 
-const testBlogs = [
-  {
-    _id: '5a422a851b54a676234d17f7',
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7,
-    __v: 0
-  },
-  {
-    _id: '5a422aa71b54a676234d17f8',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-    __v: 0
-  },
-  {
-    _id: '5a422b3a1b54a676234d17f9',
-    title: 'Canonical string reduction',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-    likes: 12,
-    __v: 0
-  },
-  {
-    _id: '5a422b891b54a676234d17fa',
-    title: 'First class tests',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-    likes: 10,
-    __v: 0
-  },
-  {
-    _id: '5a422ba71b54a676234d17fb',
-    title: 'TDD harms architecture',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-    likes: 0,
-    __v: 0
-  },
-  {
-    _id: '5a422bc61b54a676234d17fc',
-    title: 'Type wars',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-    likes: 2,
-    __v: 0
-  }
-]
+const api = supertest(app)
 
 const blogsInDb = async () => {
   const blogs = await Blog.find({})
@@ -62,9 +15,25 @@ const usersInDb = async () => {
   return users.map((user) => user.toJSON())
 }
 
+const logInTestUser = async () => {
+  const testUser = {
+    username: 'testusername',
+    password: 'testpassword123',
+    name: 'Test User'
+  }
+
+  await api.post('/api/users').send(testUser)
+
+  const loginResponse = await api.post('/api/login').send({
+    username: testUser.username,
+    password: testUser.password
+  })
+
+  return `Bearer ${loginResponse.body.token}`
+}
 
 module.exports = {
-  testBlogs,
   blogsInDb,
-  usersInDb
+  usersInDb,
+  logInTestUser
 }
