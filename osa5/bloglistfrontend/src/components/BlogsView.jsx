b0c50f593a09c8c7b8bbb708096822fed5e4fd00
Otto Vayrynen <otto.vayrynen@gmail.com>
2026-03-14 01:47:49 +0200

    5.10

--- osa5/bloglistfrontend/src/components/BlogsView.jsx
+++ osa5/bloglistfrontend/src/components/BlogsView.jsx
@@ -2,17 +2,21 @@ import Blog from './Blog'
 import NewBlogForm from './NewBlogForm'
 import Togglable from './Togglable'
 
-const BlogsView = ({ name, blogs, handleLogout, handleLikeUpdate, onCreate, blogFormRef }) => (
+const BlogsView = ({ name, blogs, handleLogout, handleLikeUpdate, onCreate, blogFormRef }) => {
+  const sortedBlogs = [...blogs].sort((a, b) => b.likes - a.likes)
+
+  return (
     <div>
       <h2>blogs</h2>
       <p>{name} logged in <button onClick={handleLogout}>logout</button></p>
-      {blogs.map(blog =>
+      {sortedBlogs.map(blog =>
         <Blog blog={blog} handleLikeUpdate={handleLikeUpdate} />
       )}
       <Togglable buttonLabel="create new blog" ref={blogFormRef}>
         <NewBlogForm onCreate={onCreate} />
       </Togglable>
     </div>
-)
+  )
+}
 
 export default BlogsView
