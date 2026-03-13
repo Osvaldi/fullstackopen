3fcbf9525b252103ae5775d6485e546a7e7f0468
Otto Vayrynen <otto.vayrynen@gmail.com>
2026-03-14 01:02:21 +0200

    5.7

--- osa5/bloglistfrontend/src/components/Blog.jsx
+++ osa5/bloglistfrontend/src/components/Blog.jsx
@@ -1,7 +1,35 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+
+const Blog = ({ blog }) => {
+  const [showContent, setShowContent] = useState(false)
+
+  const toggleContent = () => setShowContent(prev => !prev)
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button onClick={toggleContent}>{showContent ? 'hide' : 'view'}</button>
+      {showContent && (
+        <>
+          <br />
+          {blog.url}
+          <br />
+          likes {blog.likes} <button>like</button>
+          <br />
+          {blog.user.name}
+        </>
+      )}
+    </div>
+  )
+}
 
 export default Blog
