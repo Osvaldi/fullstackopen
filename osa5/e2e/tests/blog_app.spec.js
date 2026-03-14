570e4f262e523b7cdb9199aed6a1c04907bf46a5
Otto Vayrynen <otto.vayrynen@gmail.com>
2026-03-15 01:09:08 +0200

    5.18

--- osa5/e2e/tests/blog_app.spec.js
+++ osa5/e2e/tests/blog_app.spec.js
@@ -1,4 +1,5 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+const { loginWith } = require('./helper')
 
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
@@ -18,5 +19,16 @@ describe('Blog app', () => {
     await expect(page.getByLabel('username')).toBeVisible()
     await expect(page.getByLabel('password')).toBeVisible()
     await expect(page.getByRole('button', { name: 'login' })).toBeVisible()
+  })
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await loginWith(page, 'mluukkai', 'salainen')
+      await expect(page.getByText(`Welcome Matti Luukkainen!`)).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await loginWith(page, 'mluukkai', 'wrongpassword')
+      await expect(page.getByText('Check your username and password')).toBeVisible()
+    })
   })
 })
