0214102066bf3355d5c1b8c23f2a584dee2ca165
Otto Vayrynen <otto.vayrynen@gmail.com>
2026-03-17 00:30:22 +0200

    6.14

--- osa6/redux-anecdotes/src/App.jsx
+++ osa6/redux-anecdotes/src/App.jsx
@@ -1,9 +1,21 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
 
+import { setAnecdotes } from './reducers/anecdoteReducer'
+import AnecdoteService from './services/anecdotes'
+
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    AnecdoteService.getAll().then(anecdotes => dispatch(setAnecdotes(anecdotes)))
+  }, [dispatch])
+
   return (
     <div>
       <h2>Anecdotes</h2>
